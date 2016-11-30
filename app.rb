1a70a3d9b6d9bb545d48131a6e6f4127adab4fb1
Neil Barduson <barduson.neil@gmail.com>
2016-11-30 13:55:33 -0600

    minor mods, added comments

--- app.rb
+++ app.rb
@@ -6,14 +6,16 @@ require 'gon-sinatra'
 
 Sinatra::register Gon::Sinatra
 
+#read from the database json file
 ncaaf = File.read('db.json')
+#list of NCAA team documents
 teams = JSON.parse(ncaaf)['teams']
 
 get '/map/:team' do
+	#get the teams database document
 	team = teams.find { |x| x['abbr'] == params[:team].upcase }
+	#set the erb players variable, its a list of player json objects
 	gon.players = team['roster']
 
 	erb :map
-
-	#make html injection string/code
 end
